edc238ec8b7da09f1f7a1b9c2f287ee3da1ee617
zharickcano <zharick_cano82222@elpoli.edu.co>
2026-03-26 15:57:44 -0500

    correccion errores

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
-//(Errores: Evento mal escuchado, falta preventDefault, captura de datos errónea)
-//codeJavaScript
-// FALLA: El evento debe esperar a que el DOM cargue o estar al final del HTML
+
 const form = document.getElementById("formulario-registro");
 const lista = document.getElementById("lista-usuarios");
 
 form.addEventListener("submit", function(e)  {
- // FALLA: Falta evitar que la página se recargue
+
   e.preventDefault();
 
  const id = document.getElementById("id_usuario").value;
@@ -15,31 +13,20 @@ form.addEventListener("submit", function(e)  {
 
 if(id === "" || nombre === "" || genero === "") {
         alert("Por favor, llene todos los campos");
-        console.log('no vacio')
+        console.log('vacio')
         return
     } else {
-        // TAREA: Crear el elemento para mostrar los datos
+       
         const info = document.createElement("p");
         info.innerHTML = `<strong>Registrado:</strong> ${nombre} ${apellido} (${genero}) - ID: ${id}`;
        
-        lista.innerHTML ="";
         lista.appendChild(info);
         
-        // Limpiar formulario
+        
         form.reset();
     }
 
- /*FALLA: ¿Cómo capturo el género si el select no tiene ID?
- if (genero === ""){ 
-    console.log("el genero es" + (genero));
- }
- if(id == "") {
- console.log("Error");
- } else {
- // TAREA: Crear un elemento en el HTML para mostrar los datos
- console.log("Datos capturados");
- }
-procesarRegistro();*/
+
 
 });
 
